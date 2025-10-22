ac992feaef40e449a6a3df1b7928e8ba6defd315
Patrick Labbett <patrick@labbett.net>
2025-10-22 11:56:34 -0400

    Adding genesis queue to config

--- config/horizon.php
+++ config/horizon.php
@@ -167,7 +167,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default', 'transcriptions', 'inbound-email', 'outbound-email', 'ringcentral', 'mfax', 'better-emails', 'ffmpeg', 'sox', 'people-praise', 'copia'],
+            'queue' => ['default', 'transcriptions', 'inbound-email', 'outbound-email', 'ringcentral', 'mfax', 'better-emails', 'ffmpeg', 'sox', 'people-praise', 'copia', 'genesis'],
             'balance' => 'auto',
             'maxProcesses' => 1,
             'memory' => 128,
